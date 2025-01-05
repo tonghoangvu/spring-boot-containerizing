3a95271c1b33e6af6e9fe07f41a7688963649912
Vu Tong <tonghoangvu@outlook.com>
2025-01-05 22:37:14 +0700

    Init Spring Boot project

---
+++ src/main/java/vn/id/tonghoangvu/springbootcontainerizing/SpringBootContainerizingApplication.java
@@ -0,0 +1,13 @@
+package vn.id.tonghoangvu.springbootcontainerizing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootContainerizingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootContainerizingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/id/tonghoangvu/springbootcontainerizing/SpringBootContainerizingApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.id.tonghoangvu.springbootcontainerizing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootContainerizingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
